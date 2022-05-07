3b2ab3f9351ec6925ea4781e75205c936ec5d6ec
XiangRongLin <41164160+XiangRongLin@users.noreply.github.com>
2022-05-07 17:53:49 +0200

    Ignore errors of first docker-compose up

--- deployment/redeploy.sh
+++ deployment/redeploy.sh
@@ -9,7 +9,7 @@ fi
 
 git pull
 docker-compose down
-docker-compose up -d
+docker-compose up -d || :
 # For some reason the first time fails, because the seccomp is not getting read properly.
 # It fails with filename too long. The container is created, but not startet, so we just do it again.
 docker-compose up -d 
