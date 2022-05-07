6c6d61f3725af7ef8f350a12c56d05d0c382d114
XiangRongLin <41164160+XiangRongLin@users.noreply.github.com>
2022-05-07 15:24:20 +0200

    finish

--- deployment/aws-linux-init.sh
+++ deployment/aws-linux-init.sh
@@ -15,17 +15,40 @@ curl -L https://github.com/docker/compose/releases/latest/download/docker-compos
 chmod +x /usr/local/bin/docker-compose
 
 # webhook
-curl -L https://github.com/adnanh/webhook/releases/latest/download/webhook-linux-amd64.tar.gz -o /usr/local/bin/webhook-linux-amd64.tar.gz
-tar -xzf /usr/local/bin/webhook-linux-amd64.tar.gz
-mv /usr/local/bin/webhook-linux-amd64/webhook /usr/local/bin
-rm /usr/local/bin/webhook-linux-amd64.tar.gz
-rm -d /usr/local/bin/webhook-linux-amd64
+curl -L https://github.com/adnanh/webhook/releases/latest/download/webhook-linux-amd64.tar.gz -o /tmp/webhook-linux-amd64.tar.gz
+tar -xzf /tmp/webhook-linux-amd64.tar.gz -C /tmp
+mv /tmp/webhook-linux-amd64/webhook /usr/local/bin
+rm /tmp/webhook-linux-amd64.tar.gz
+rm -d /tmp/webhook-linux-amd64
 
 # application
 mkdir /app
 git clone https://github.com/NovelService/NovelWorkerNode.git /app
 ## todo register webhook as service https://davidauthier.com/blog/2017/09/07/deploy-using-github-webhooks/
 
-## todo set env variables and webhook secret
+cat >> /etc/systemd/system/webhook.service<< EOF
+[Unit]
+Description=Webhooks
+
+[Service]
+ExecStart=/usr/local/bin/webhook -hooks /app/deployment/hooks.json -hotreload
+
+[Install]
+WantedBy=multi-user.target
+EOF
+systemctl enable webhook.service
+systemctl start webhook.service
+
+cat >> /app/deployment/.env <<EOF
+ACCESS_KEY_ID=
+SECRET_ACCESS_KEY=
+REGION=
+QUEUE_URL=
+BUCKET=
+POLL_INTERVAL=
+TABLE=
+EOF
+
+echo "" > /app/deployment/WEBHOOK_SECRET_SHA_1
 
 docker-compose -f /app/deployment/docker-compose.yml up -d  
