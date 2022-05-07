9fb29508179be20fd41ca02e81378b1e61c01358
XiangRongLin <41164160+XiangRongLin@users.noreply.github.com>
2022-05-07 15:38:11 +0200

    Add vm init scripts and webhooks to deploy application

---
+++ deployment/aws-linux-init.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+# git
+yum install -y git
+
+# docker
+amazon-linux-extras install -y docker
+service docker start
+usermod -a -G docker ec2-user
+## autostart docker
+chkconfig docker on
+
+# docker-compose
+curl -L https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+
+# webhook
+curl -L https://github.com/adnanh/webhook/releases/latest/download/webhook-linux-amd64.tar.gz -o /tmp/webhook-linux-amd64.tar.gz
+tar -xzf /tmp/webhook-linux-amd64.tar.gz -C /tmp
+mv /tmp/webhook-linux-amd64/webhook /usr/local/bin
+rm /tmp/webhook-linux-amd64.tar.gz
+rm -d /tmp/webhook-linux-amd64
+
+# application
+mkdir /home/ec2-user/app
+git clone https://github.com/NovelService/NovelWorkerNode.git /home/ec2-user/app
+
+# start webhook as service
+cat >> /etc/systemd/system/webhook.service<< EOF
+[Unit]
+Description=Webhooks
+
+[Service]
+ExecStart=/usr/local/bin/webhook -hooks /home/ec2-user/app/deployment/hooks.json -hotreload
+
+[Install]
+WantedBy=multi-user.target
+EOF
+systemctl enable webhook.service
+systemctl start webhook.service
+
+# prepare secrets
+cat >> /home/ec2-user/app/deployment/.env <<EOF
+ACCESS_KEY_ID=
+SECRET_ACCESS_KEY=
+REGION=
+QUEUE_URL=
+BUCKET=
+POLL_INTERVAL=
+TABLE=
+EOF
+
+touch /home/ec2-user/app/deployment/WEBHOOK_SECRET_SHA_1
+
+# start application
+docker-compose -f /home/ec2-user/app/deployment/docker-compose.yml up -d
