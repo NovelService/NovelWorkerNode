6168bd47bbbd80288e4c85905e9fd7ad93d3e7da
XiangRongLin <41164160+XiangRongLin@users.noreply.github.com>
2022-05-09 22:05:47 +0200

    Set url validity to one day

--- src/fileHandler.ts
+++ src/fileHandler.ts
@@ -1,13 +1,14 @@
-import { Context } from "./types/context.js";
-import { HttpRequest } from "@aws-sdk/protocol-http";
-import { parseUrl } from "@aws-sdk/url-parser";
-import { GetObjectCommand, PutObjectCommand } from "@aws-sdk/client-s3";
-import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
-import { formatUrl } from "@aws-sdk/util-format-url";
+import { Context } from "./types/context.js"
+import { HttpRequest } from "@aws-sdk/protocol-http"
+import { parseUrl } from "@aws-sdk/url-parser"
+import { PutObjectCommand } from "@aws-sdk/client-s3"
+import { formatUrl } from "@aws-sdk/util-format-url"
 import { readFileSync } from 'fs'
 
+const oneDay = 86400
+
 async function saveFile(context: Context, filename: string) {
-    const body = readFileSync("./" + filename);
+    const body = readFileSync("./" + filename)
     const command = new PutObjectCommand(
         {
             Bucket: context.config.aws.s3.bucket,
@@ -22,9 +23,9 @@ async function saveFile(context: Context, filename: string) {
 
 async function createPresignedUrl(context: Context, filename: string): Promise<string> {
     // From https://aws.amazon.com/blogs/developer/generate-presigned-url-modular-aws-sdk-javascript/
-    const unsignedUrl = parseUrl(`https://${context.config.aws.s3.bucket}.s3.${context.config.aws.region}.amazonaws.com/${filename}`);
-    const signedUrl =  await context.clients.s3Presigner.presign(new HttpRequest(unsignedUrl));
-    return formatUrl(signedUrl);
+    const unsignedUrl = parseUrl(`https://${context.config.aws.s3.bucket}.s3.${context.config.aws.region}.amazonaws.com/${filename}`)
+    const signedUrl = await context.clients.s3Presigner.presign(new HttpRequest(unsignedUrl), { expiresIn: oneDay})
+    return formatUrl(signedUrl)
 }
 
-export default { saveFile, createPresignedUrl };
+export default { saveFile, createPresignedUrl }
