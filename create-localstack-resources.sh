bd8121ef9e09d68db9489ffa24589e2c96ef83c8
Xiang Rong Lin <41164160+XiangRongLin@users.noreply.github.com>
2022-05-22 11:15:51 +0200

    export dummy aws credentials

--- create-localstack-resources.sh
+++ create-localstack-resources.sh
@@ -2,6 +2,9 @@
 set -eux
 
 export AWS_DEFAULT_REGION=us-east-1
+export AWS_ACCESS_KEY_ID=test
+export AWS_SECRET_ACCESS_KEY=test
+
 aws --endpoint-url=http://localhost:4566 sqs create-queue --queue-name queue
 aws --endpoint-url=http://localhost:4566 s3api create-bucket --bucket bucket
 aws --endpoint-url=http://localhost:4566 dynamodb create-table --table-name \
