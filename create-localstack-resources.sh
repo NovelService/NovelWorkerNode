434fe048dc8a7410eeb6f7721e66d3a5d485e2cb
Xiang Rong Lin <41164160+XiangRongLin@users.noreply.github.com>
2022-05-22 11:42:53 +0200

    Add aws cli debug flag

--- create-localstack-resources.sh
+++ create-localstack-resources.sh
@@ -5,9 +5,9 @@ export AWS_DEFAULT_REGION=us-east-1
 export AWS_ACCESS_KEY_ID=test
 export AWS_SECRET_ACCESS_KEY=test
 
-aws --endpoint-url=http://127.0.0.1:4566 sqs create-queue --queue-name queue
-aws --endpoint-url=http://127.0.0.1:4566 s3api create-bucket --bucket bucket
+aws --endpoint-url=http://127.0.0.1:4566 sqs create-queue --queue-name queue --debug
+aws --endpoint-url=http://127.0.0.1:4566 s3api create-bucket --bucket bucket  --debug
 aws --endpoint-url=http://127.0.0.1:4566 dynamodb create-table --table-name \
   table --attribute-definitions AttributeName=id,AttributeType=S \
   --key-schema AttributeName=id,KeyType=HASH \
-  --provisioned-throughput ReadCapacityUnits=5,WriteCapacityUnits=5
+  --provisioned-throughput ReadCapacityUnits=5,WriteCapacityUnits=5 --debug
